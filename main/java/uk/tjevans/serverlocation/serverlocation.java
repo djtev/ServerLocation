58905914234520e8644e961722f8a1f6a7649823
skyerzz <spammen67@gmail.com>
2017-02-16 19:17:54 +0100

    added thread to issue /whereami every 10 seconds

--- main/java/uk/tjevans/serverlocation/serverlocation.java
+++ main/java/uk/tjevans/serverlocation/serverlocation.java
@@ -13,58 +13,77 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 
 @Mod(modid = "server location", version = "0.1", acceptedMinecraftVersions = "[1.8]")
-public class serverlocation
-{
+public class serverlocation{
 
-  private static final Minecraft MINECRAFT = Minecraft.getMinecraft();
-  private boolean isOnHypixel;
-  public String server;
+    private static final Minecraft MINECRAFT = Minecraft.getMinecraft();
+    private boolean isOnHypixel = false;
+    private String server = "NULL";
+    private Thread thread = null;
+    private int timeOut = 10000;
 
-  @EventHandler
-  public void init(FMLInitializationEvent event) {
-    MinecraftForge.EVENT_BUS.register(this);
-    FMLCommonHandler.instance().bus().register(this);
-  }
+    @EventHandler
+    public void init(FMLInitializationEvent event){
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        Thread.sleep(timeOut);
+                        issueLocationCommand();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        MinecraftForge.EVENT_BUS.register(this);
+        FMLCommonHandler.instance().bus().register(this);
+    }
   
-  @SubscribeEvent
-    public void checkChat(ClientChatReceivedEvent e)
-	{
-		 if(e.type!=0)
-        {
+    @SubscribeEvent
+    public void checkChat(ClientChatReceivedEvent e){
+        if(e.type!=0){
             return;
         }
         String chat = e.message.getUnformattedText().replaceAll("\u00A7.", "");
-        if(!chat.contains("You are currently on server"))
-        {
+        if(!chat.contains("You are currently on server")){
             return;
         }
         String[] split = chat.split(" ");
         server = split[4];
-	}
+    }
 
     @SubscribeEvent
-  public void RenderGameOverlayEvent(RenderGameOverlayEvent e) {
-    if (!isOnHypixel || !MINECRAFT.inGameHasFocus){
-      return;
+    public void RenderGameOverlayEvent(RenderGameOverlayEvent e) {
+        if (!isOnHypixel || !MINECRAFT.inGameHasFocus){
+            return;
+        }
+        MINECRAFT.fontRendererObj.drawStringWithShadow("Instance: " + server, 10, 10, 0xC838FC);
     }
-	MINECRAFT.fontRendererObj.drawStringWithShadow("Instance: " + server, 10, 10, 0xC838FC);
-	}
   
-  @SubscribeEvent
-  public void onConnect(FMLNetworkEvent.ClientConnectedToServerEvent e) {
-    final ServerData data = MINECRAFT.getCurrentServerData();
-    if (data != null && data.serverIP.contains("hypixel.net")) {
-      isOnHypixel = true;
+    @SubscribeEvent
+    public void onConnect(FMLNetworkEvent.ClientConnectedToServerEvent e) {
+        final ServerData data = MINECRAFT.getCurrentServerData();
+        if (data != null && data.serverIP.contains("hypixel.net")) {
+            isOnHypixel = true;
+            startServerCheckThread();
+        }
     }
-  }
 
-  @SubscribeEvent
-  public void onDisconnect(FMLNetworkEvent.ClientDisconnectionFromServerEvent e) {
+    private void startServerCheckThread(){
+        thread.start();
+    }
+
+    private void stopServercheckThread(){
+        thread.interrupt();
+    }
+
+    @SubscribeEvent
+    public void onDisconnect(FMLNetworkEvent.ClientDisconnectionFromServerEvent e) {
     isOnHypixel = false;
-  }
-  
-  @EventHandler
-  public void onServerConnect() {
+    }
+
+    public void issueLocationCommand() {
 	  MINECRAFT.thePlayer.sendChatMessage("/whereami");
 	}
 }
